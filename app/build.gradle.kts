7c4cfabace5259f76e24b907e437f46d08fde03b
Mesh_Hun <2318923367@qq.com>
2026-05-21 23:14:10 +0800

    release1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "hun.mesh.carwithenhance"
         minSdk = 29
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
     }
 
     buildTypes {
